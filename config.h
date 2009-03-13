ce9b02e891878ed74913abca2811f03bec23051c
Peter Zion <peter.zion@gmail.com>
2009-03-13 19:48:42 -0400

    Sort out project mess

---
+++ config.h
@@ -0,0 +1,11 @@
+/*
+ *  config.h
+ *  CercaMap
+ *
+ *  Created by Peter Zion on 13/03/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#define VIRTUAL_EARTH_KIT_USERNAME "137913"
+#define VIRTUAL_EARTH_KIT_PASSWORD "!panChr0mat1c7"
